c08e802b328a093b65cf6f488aea3057e205cebc
Ashton Meuser <ashtonmeuser@gmail.com>
2020-02-10 10:35:12 -0800

    Revert simplified app run

--- server/manage.py
+++ server/manage.py
@@ -1,7 +1,9 @@
+from flask.cli import FlaskGroup
 from app import create_app, db
 from app.api.models.foo import Foo
 
 app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 if __name__ == '__main__':
-    app.run()
+    cli()
