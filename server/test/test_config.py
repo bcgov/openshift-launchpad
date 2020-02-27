06ab741c327c482c0f793e05c7635e793fea3d9e
Ashton Meuser <ashtonmeuser@gmail.com>
2020-02-27 12:34:52 -0800

    Fix linting issues

--- server/test/test_config.py
+++ server/test/test_config.py
@@ -1,29 +1,34 @@
-import os
-import unittest
+"""
+Test app config obejcts
+"""
+
 from flask import current_app
 from flask_testing import TestCase
 from app import create_app
 
-app = create_app()
+APP = create_app()
 
 class TestConfig(TestCase):
+    """Test standard app config"""
     def create_app(self):
-        app.config.from_object('app.config.Config')
-        return app
+        """Create app object from standard config"""
+        APP.config.from_object('app.config.Config')
+        return APP
 
     def test_config_works(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'temp')
+        """Test config properties"""
+        self.assertTrue(APP.config['SQLALCHEMY_DATABASE_URI'])
         self.assertFalse(current_app is None)
 
 class TestTestConfig(TestCase):
+    """Test testing app config"""
     def create_app(self):
-        app.config.from_object('app.config.TestConfig')
-        return app
+        """Create app object from test config"""
+        APP.config.from_object('app.config.TestConfig')
+        return APP
 
     def test_app_is_testing(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'temp')
-        self.assertTrue(app.config['TESTING'])
-        self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
-
-if __name__ == '__main__':
-    unittest.app()
+        """Test config properties"""
+        self.assertTrue(APP.config['SQLALCHEMY_DATABASE_URI'])
+        self.assertTrue(APP.config['TESTING'])
+        self.assertFalse(APP.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
